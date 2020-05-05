863131cfabf4981e8a8f7852d3ab8de146f97316
Jackson Smith <jacksonsmith7343@gmail.com>
2020-05-05 14:15:19 -0500

    Added classes for the coins.

---
+++ Soda_Machine_Project/Dime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soda_Machine_Project
+{
+    class Dime
+    {
+        //member variables (has a)
+
+        //constructor (spawner)
+        public Dime()
+        {
+
+        }
+
+        //member methods (can do)
+    }
+}
---
+++ Soda_Machine_Project/Penny.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soda_Machine_Project
+{
+    class Penny
+    {
+        //member variables (has a)
+
+        //constructor (spawner)
+        public Penny()
+        {
+
+        }
+
+        //member methods (can do)
+    }
+}
